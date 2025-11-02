eed285e48822f1d532ddd3bfcf65c757ed87071d
irineu <mmmirineusilva@gmail.com>
2025-11-02 07:29:07 -0300

    chore(version): release v2.3.0

--- build-logic/src/main/kotlin/extension/Project.kt
+++ build-logic/src/main/kotlin/extension/Project.kt
@@ -7,7 +7,7 @@ val config = Config(
         major = 2,
         minor = 3,
         patch = 0,
-        release = Config.Release.Snapshot(number = 16)
+        release = Config.Release.Definitive,
     ),
     android = Config.Android(
         compileSdk = 34,
